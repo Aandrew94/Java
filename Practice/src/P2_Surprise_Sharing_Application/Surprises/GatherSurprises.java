4a8013646b0aaf2c5197fa98d4b26dbfc6fdc422
Andrew <62358136+Aandrew94@users.noreply.github.com>
2021-09-13 19:35:52 +0300

    Update Surprises

--- Practice/src/P2_Surprise_Sharing_Application/Surprises/GatherSurprises.java
+++ Practice/src/P2_Surprise_Sharing_Application/Surprises/GatherSurprises.java
@@ -1,7 +1,7 @@
 package P2_Surprise_Sharing_Application.Surprises;
 
 import java.util.Random;
-
+ 
 public final class GatherSurprises {
     private static  final Random    random  =   new Random();
     private GatherSurprises(){}
