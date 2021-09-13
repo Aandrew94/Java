bf3359df56c25fe7cfa46c3de6886ee9939146bc
Andrew <62358136+Aandrew94@users.noreply.github.com>
2021-09-13 19:34:59 +0300

    Update GiveSurprises

--- Practice/src/P2_Surprise_Sharing_Application/GiveSurprises/GiveSurpriseAndApplause.java
+++ Practice/src/P2_Surprise_Sharing_Application/GiveSurprises/GiveSurpriseAndApplause.java
@@ -2,7 +2,7 @@ package P2_Surprise_Sharing_Application.GiveSurprises;
 
 public class GiveSurpriseAndApplause extends AbstractGiveSurprises{
     public GiveSurpriseAndApplause(String type, int time){  super(type,time);   }
-
+ 
     @Override
     void giveWithPassion() {
         System.out.println("Loud applause to you… For it is in giving that we receive.\n");
